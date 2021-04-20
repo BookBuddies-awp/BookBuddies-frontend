7ba10357c300c7126e95b0a51ce48d2346801d59
Harshal <harshalmaniya26@gmail.com>
2021-04-20 11:45:13 +0530

    Top Search

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const fetch = require('node-fetch');
 async function Recommended(){
 const response= await fetch('https://bookbuddies-api.herokuapp.com/api/bestsellers',{headers:{
     'Authorization':'x0G4Q1Rceqa907jhTmrD'
-},})/*.then(res => res.json())
-.then(json => {
-    console.log(json[0]);
-    console.log("Name of the first Book in the array:");
-    console.log(json[0].title);
-    
-});*/
+},})
 return response.json();}
-Recommended().then(data=>{console.log(data)});
+Recommended().then(data=>{console.log(data[0])});
+async function TopSearch(){
+    const response= await fetch('https://bookbuddies-api.herokuapp.com/api/top-searched',{headers:{
+        'Authorization':'x0G4Q1Rceqa907jhTmrD'
+    },})
+    return response.json();}
