158c4198c4ccd9af11efb994b1045e740483ddd0
Harshal <harshalmaniya26@gmail.com>
2021-04-20 11:42:22 +0530

    Recommended async

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 console.log("Hello World");
 const fetch = require('node-fetch');
-function Recommended(){
-fetch('https://bookbuddies-api.herokuapp.com/api/bestsellers',{headers:{
+async function Recommended(){
+const response= await fetch('https://bookbuddies-api.herokuapp.com/api/bestsellers',{headers:{
     'Authorization':'x0G4Q1Rceqa907jhTmrD'
-},}).then(res => res.json())
+},})/*.then(res => res.json())
 .then(json => {
     console.log(json[0]);
     console.log("Name of the first Book in the array:");
     console.log(json[0].title);
-});}
-Recommended();
+    
+});*/
+return response.json();}
+Recommended().then(data=>{console.log(data)});
