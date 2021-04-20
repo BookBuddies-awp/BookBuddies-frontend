ad84c1063daaab2f91351d5d3b245b0fd828f170
Harshal <harshalmaniya26@gmail.com>
2021-04-20 11:36:03 +0530

    Recommended data fetch

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("Hello World");
+const fetch = require('node-fetch');
+function Recommended(){
+fetch('https://bookbuddies-api.herokuapp.com/api/bestsellers',{headers:{
+    'Authorization':'x0G4Q1Rceqa907jhTmrD'
+},}).then(res => res.json())
+.then(json => {
+    console.log(json[0]);
+    console.log("Name of the first Book in the array:");
+    console.log(json[0].title);
+});}
+Recommended();
