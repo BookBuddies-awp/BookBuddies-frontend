788a180ee1c12ed7409e12ca75fed9ff7e9dd895
Harshal <harshalmaniya26@gmail.com>
2021-04-22 12:14:20 +0530

    Recommendation Page Working

--- app/recommended.js
+++ app/recommended.js
@@ -5,13 +5,10 @@ app.controller('mycontroller', async function ($scope, $http) {
     method: 'GET',
     url: 'https://bookbuddies-api.herokuapp.com/api/bestsellers',
     headers: { "Authorization": "x0G4Q1Rceqa907jhTmrD" }
-  })
-
-    .then(function (response) {
-
-      console.log("elo" + response.data[0]);
-      $scope.myWelcome = response.data[0].title;
-    });
+  }).then(function (response) {
+    $scope.books = response.data;
+    console.log($scope.books)
+  });
 });
 
 
