a1bce2f77aeab8f73cd980875e68f83c7bd52837
Harshal <harshalmaniya26@gmail.com>
2021-04-25 14:10:57 +0530

    Search Endpoint

--- app/services/services.js
+++ app/services/services.js
@@ -18,6 +18,18 @@ app.service('booksService', function ($http) {
       return books;
     });
   };
+  
+  searchBooks = function (name) {
+    return $http({
+      method: 'GET',
+      url: 'https://bookbuddies-api.herokuapp.com/api/search?q=${name}',
+      headers: { Authorization: 'x0G4Q1Rceqa907jhTmrD' },
+    }).then(function (response) {
+      books = response.data;
+      console.log(books);
+      return books;
+    });
+  };
 
   onSelectBook = function (book) {
     selectedBook = book;
