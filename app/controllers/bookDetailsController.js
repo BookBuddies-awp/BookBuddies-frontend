fe2b5bbe4944c9ded637210490f91c9529c7666d
Harshal <harshalmaniya26@gmail.com>
2021-04-27 10:47:41 +0530

    Dynamic Ratings

--- app/controllers/bookDetailsController.js
+++ app/controllers/bookDetailsController.js
@@ -2,5 +2,10 @@ var app = angular.module('myApp');
 
 app.controller('bookDetailsController', function ($scope, booksService) {
   $scope.book = booksService.getSelectedBook();
+  
+  $scope.ratings=Array(Math.ceil($scope.book.ratings));
+  for(let i=0;i<(Math.ceil($scope.book.ratings));i++){
+    $scope.ratings[i]=i;
+  }
   console.log($scope.book);
 });
