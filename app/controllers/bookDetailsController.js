b8f550731cf8b7b98147391a3dc1d63d9a67c6ae
Harshal <harshalmaniya26@gmail.com>
2021-04-27 12:12:10 +0530

    NO ratings provided fix

--- app/controllers/bookDetailsController.js
+++ app/controllers/bookDetailsController.js
@@ -2,10 +2,14 @@ var app = angular.module('myApp');
 
 app.controller('bookDetailsController', function ($scope, booksService) {
   $scope.book = booksService.getSelectedBook();
-  
-  $scope.ratings=Array(Math.ceil($scope.book.ratings));
-  for(let i=0;i<(Math.ceil($scope.book.ratings));i++){
-    $scope.ratings[i]=i;
+  if ($scope.book.ratings === undefined) {
+    $scope.ratings = Array[0]
+  }
+  else {
+    $scope.ratings = Array(Math.ceil($scope.book.ratings));
+    for (let i = 0; i < (Math.ceil($scope.book.ratings)); i++) {
+      $scope.ratings[i] = i;
+    }
   }
   console.log($scope.book);
 });
